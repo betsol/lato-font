530f946cdb2dcbe70d9ef23e9ed9defe3c3c0cb7
Slava Fomin II <s.fomin@betsol.ru>
2017-02-28 18:24:19 +0300

    Refactored Gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,35 @@
+
 //--------------//
 // DEPENDENCIES //
 //--------------//
 
-var gulp = require('gulp');
-var del = require('del');
-var runSequence = require('run-sequence');
-var compass = require('gulp-compass');
-var rename = require("gulp-rename");
-var minifyCSS = require('gulp-minify-css');
+const gulp = require('gulp');
+const gulpRequireTasks = require('gulp-require-tasks');
+const del = require('del');
+const runSequence = require('run-sequence');
+
 
 //---------------//
 // CONFIGURATION //
 //---------------//
 
-var cssDir = './css';
-var scssDir = './scss';
-var miniSuffix = '.min';
+global.CSS_OUTPUT_PATH = './css';
+global.SCSS_SOURCE_PATH = './scss';
+global.MINIFICATION_SUFFIX = '.min';
+
 
 //-------//
 // TASKS //
 //-------//
 
+gulpRequireTasks();
+
 gulp.task('default', function (callback) {
   runSequence('clean', 'build', callback);
 });
 
-gulp.task('clean', function (callback) {
-  del([cssDir + '/*'], callback);
+gulp.task('clean', function () {
+  return del([CSS_OUTPUT_PATH + '/*']);
 });
 
-gulp.task('build', ['build:styles']);
-
-gulp.task('build:styles', function () {
-
-  var compassConfig = {
-    css: cssDir,
-    sass: scssDir,
-    environment: 'development',
-    style: 'expanded',
-    comments: true
-  };
-
-  return gulp.src(scssDir + '/*.scss')
-
-  // Writing development version.
-    .pipe(compass(compassConfig))
-    .pipe(gulp.dest(cssDir))
-
-    // Writing minified version.
-    .pipe(rename({suffix: miniSuffix}))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest(cssDir))
-    ;
-});
+gulp.task('build', ['styles:build']);
